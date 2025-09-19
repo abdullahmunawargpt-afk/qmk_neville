7873b02f3e3a13895fa84a543923037ccc06b318
abdullahmunawargpt-afk <abdullahmunawar.gpt@gmail.com>
2025-09-19 06:16:55 +0500

    Update config.h

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/config.h
@@ -36,18 +36,36 @@
 #define NO_ACTION_ONESHOT
 #endif // __arm__
 
-/* Charybdis-specific features. */
+/* ---------------- Charybdis-specific features ---------------- */
 
 #ifdef POINTING_DEVICE_ENABLE
-#define POINTING_DEVICE_SCROLL_ENABLE
-#define POINTING_DEVICE_INVERT_V
-// Automatically enable the pointer layer when moving the trackball.  See also:
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
-// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+    #define POINTING_DEVICE_SCROLL_ENABLE
+    #define POINTING_DEVICE_INVERT_V
+    // Automatically enable the pointer layer when moving the trackball.
+    // #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+    // #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS ...
+    // #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD ...
 #endif // POINTING_DEVICE_ENABLE
 
-#define COMBO_COUNT 3 // Change this number.
+/* ----- Dual pointing device configuration ----- */
+
+/* Cirque Pinnacle (trackpad on left half) */
+#define CIRQUE_PINNACLE_POSITION_MODE PSM_RELATIVE
+#define CIRQUE_PINNACLE_I2C_ADDRESS   0x2A   // change to 0x2B if jumpers are set
+#define CIRQUE_PINNACLE_TAP_ENABLE    0
+#define CIRQUE_PINNACLE_ATTENUATION   1
+
+/* PMW3360 (trackball on right half) */
+#define PMW3360_CPI_DEFAULT 1600
+#define PMW3360_CPI_MIN     200
+#define PMW3360_CPI_MAX     6400
+
+/* Combined pointing device timing */
+#define POINTING_DEVICE_TASK_THROTTLE_MS 1
+
+/* -------------------------------------------------------------- */
+
+#define COMBO_COUNT 3 // Change this number as needed
 #define COMBO_TERM 120
 
 //#define RETRO_SHIFT 500
@@ -55,7 +73,7 @@
 
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
-#define NO_ACTION_ONESHOT
+#define NO_ACTION_ONESHOT   // keep only one definition
 
 //#define IGNORE_MOD_TAP_INTERRUPT
 #define HOLD_ON_OTHER_KEY_PRESS
@@ -64,4 +82,4 @@
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
 
 //#define CHARYBDIS_DRAGSCROLL_REVERSE_X
-#define CHARYBDIS_DRAGSCROLL_REVERSE_Y
+//#define CHARYBDIS_DRAGSCROLL_REVERSE_Y
